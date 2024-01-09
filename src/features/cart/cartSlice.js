2c0289f1a0d2e8a0338e9612e3ea85324027564b
Atakan <atakan.dogan@gmail.com>
2024-01-09 08:36:54 +0300

    318 - Building the Cart Page

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,8 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  //cart: [],
+  cart: [],
 
+  /*
   cart: [
     {
       pizzaId: 12,
@@ -12,6 +13,7 @@ const initialState = {
       totalPrice: 32,
     },
   ],
+  */
 };
 
 const cartSlice = createSlice({
@@ -53,3 +55,11 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
