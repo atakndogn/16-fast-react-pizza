e5fbcf2b420affc0a8f287fb84f21e38e5ad5f49
Atakan <atakan.dogan@gmail.com>
2024-01-14 08:46:28 +0300

    320 - Updating Cart Quantities

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -3,6 +3,7 @@ import Button from '../../ui/Button';
 import { useDispatch, useSelector } from 'react-redux';
 import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
 import DeleteItem from '../cart/DeleteItem';
+import UpdateItemQuantity from '../cart/UpdateItemQuantity';
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
@@ -45,7 +46,13 @@ function MenuItem({ pizza }) {
           )}
 
           {currentQuantity > 0 ? (
-            <DeleteItem pizzaId={id} />
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItem pizzaId={id} />
+            </div>
           ) : (
             !soldOut && (
               <Button type="small" onClick={handleAddToCart}>
